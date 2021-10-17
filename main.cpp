ca97b134b94ec863011e81f32577641bfe60713e
Vishwajeet Jaysing Pawar <82999574+kyler-hue@users.noreply.github.com>
2021-10-17 16:36:44 +0530

    Added the switch cases

--- main.cpp
+++ main.cpp
@@ -5,26 +5,55 @@
 
 int main()
 {
-    std::cout<<"----------- The 8 Puzzle Problem -----------"<<std::endl ;
+    std::cout<<"----------- The 8 Puzzle Problem -----------\n\n\n"<<std::endl ;
     std::cout<<"Enter your dimension value of n : "<<std::endl ;
-    int n = 0 ;
+    
     std::cin>>n ;
     int arr[n][n] ;
-    vector<vector<int>>res ;
+
     memset(arr,0,sizeof(arr)) ;
     bool valid = true ;
     res = InputPuzzle(n) ;
     change(res,(int*)arr,n) ;
-
+    
     valid = isSolvable(n,(int*)arr) ;
+    vector<vector<int>>dupliacte(res.begin(),res.end()) ;
 
     if( valid )
     {
         PrintPuzzle(res,n) ;
+        std::cout<<"\t\t\tTo Select The following Heuristic Function , Enter the Corresponding Index Number\n\n\n"<<endl ;
+        std::cin>>Choise ;
+        switch (Choise)
+        {
+            case 1://Manhattan Distance.
+                
+
+                break;
+            case 2:
+
+                
+                break;
+            case 3:
+
+                
+                break;
+            case 4:
+
+                
+                break;
+            case 5:
+
+                
+                break;
+            default:
+                break;
+        }
+
     }
     else
     {
-        std::cout<<endl<<endl<<"\nSorry ! But this puzzle can't be solved with this initial states !!! \nYou can Try again with different positions for your puzzle !!! \n Good Luck !!!"<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
+        std::cout<<endl<<endl<<"\nSorry ! But this puzzle can't be solved with this initial states !!! \n\nYou can Try again with different positions for your puzzle !!! \n\n Good Luck !!!"<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
     }
 
     return 0 ;
