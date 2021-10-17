b96b6df3175174c70b2a1d6c9920fe52470e3e70
Vishwajeet Jaysing Pawar <82999574+kyler-hue@users.noreply.github.com>
2021-10-17 16:35:53 +0530

    Update declarations.h

--- declarations.h
+++ declarations.h
@@ -8,7 +8,9 @@ void PrintPuzzle(vector<vector<int>>res , int N) ;
 void change(vector<vector<int>>&ans, int* arr ,int N) ;
 
 vector<vector<int>> InputPuzzle(int N ) ;
-
+vector<vector<int>>res ;
+int Choise ;
+int n = 0 ;
 
 
 
